33efd9e45acb07c2a1ed914961e2a0ac21aaa08b
Pushkraj001 <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>
2024-10-03 03:30:43 +0530

    added logo

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../header/header.css";
+
+const Header = () => {
+  return (
+    
+    <>
+    <header>
+        <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg"/>
+    </header>
+    
+    </>
+  )
+}
+
+export default Header
